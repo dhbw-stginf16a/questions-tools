122e4e5efc38a3ab9e679399da218525a76b419f
Thore <thore@kruess.xyz>
2017-05-21 16:06:48 +0200

    JSON export is working

--- parseQuestion.py
+++ parseQuestion.py
@@ -1,5 +1,42 @@
+#!/usr/bin/python
 import json
 import glob, os
+import sys, getopt
+
+def generateJSON(questions):
+    # Undo former chdir
+    os.chdir("..")
+    # TODO: Rework versioning
+    version = 0.1
+    exportFile = ""
+    try:
+        oldData = open("questions.json", "r")
+        oldJSON = json.load(oldData)
+        try:
+            version = oldJSON["version"]
+            version += 0.1
+        except Exception as e:
+            print("Invalid file, overwriting")
+        oldData.close()
+
+        # Remove all data from file
+        oldData = open("questions.json", "w")
+        oldData.close()
+        # Reopen to write new content
+        exportFile = open("questions.json", "w")
+    except Exception as e:
+        print(e)
+        print("No file found, will create one for you")
+        exportFile = open("questions.json", "w")
+    data = {}
+    data["version"] = version
+    data["questions"] = questions
+    jsonData = json.dumps(data)
+    exportFile.write(jsonData)
+    exportFile.close()
+
+def generatePDF(questions):
+    pass
 
 path = os.getcwd()
 path += "/questions"
@@ -10,7 +47,11 @@ for question in glob.glob("*.json"):
         data = json.loads(data_file.read())
         questions.append(data)
 
+approvedQuestions = []
 for question in questions:
+    if (question["status"] == "approved"):
+        approvedQuestions.append(question)
+
     print("Question: " + question["question"] + "\n \n")
 
     if (len(question["possibilities"]) > 0):
@@ -20,3 +61,20 @@ for question in questions:
     for answer in question["answer"]:
         print("Answer: " + answer["text"])
     print("##############################################")
+
+if(len(sys.argv) > 1):
+    opts, args = getopt.getopt(sys.argv[1:],"hj:p:",["generate-json","generate-pdf"])
+    for opt, arg in opts:
+      if (opt == '-h'):
+         print("To print all questions just run parseQuestions.py")
+         print("To generate a single json file with all questions run:")
+         print("parseQuestions.py --generate-json")
+         print("To generate a ready to print PDF with the questions run:")
+         print("parseQuestions.py --generate-pdf")
+         sys.exit()
+      elif (opt in ("-j", "--generate-json")):
+         generateJSON(approvedQuestions)
+      elif (opt in ("-p", "--generate-pdf")):
+         print("Not yet implemented -- exiting")
+         sys.exit()
+         generatePDF(approvedQuestions)
